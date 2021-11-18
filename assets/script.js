42e1f1347dec06d89e98ce0c08b3d12f70172ae7
besparza <esparzabrian90@gmail.com>
2021-11-17 19:40:40 -0800

    added prompt

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var number = ["1","2","3","4","5","6","7","8","9"]
-var special = ["!","@","#","$","%","^","&","*","(",")","/","?"]
+var acceptLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var acceptUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var acceptNumber = ["1","2","3","4","5","6","7","8","9"]
+var acceptSpecial = ["!","@","#","$","%","^","&","*","(",")","/","?"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,3 +17,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () { 
+  window.prompt("How many characters does the password need? Select between 8-128.");
+  if (password < 8 || password > 128) {
+    window.alert ("Please enter a valid number");
+    return generatePassword ();
+  }
+}
